fb07119595310212f4af2176b6bd8e8eb87912b4
jamie005 <jamiehuff2@gmail.com>
2026-04-03 21:50:39 +0000

    WIP: file caching and diff check feature

--- sync_engine/client/client.py
+++ sync_engine/client/client.py
@@ -3,6 +3,7 @@ import os
 import time
 from queue import Queue
 
+from sync_engine.client.directory_monitoring.delta_transfer_cache import DeltaTransferCache
 from sync_engine.client.directory_monitoring.directory_monitor import DirectoryMonitor
 
 
@@ -11,7 +12,9 @@ class SyncEngineClient:
 
     def __init__(self, target_directory: str) -> None:
         self._target_directory = target_directory
-        self._directory_monitor = DirectoryMonitor(target_directory, Queue())
+        self._file_events: Queue = Queue()
+        self._directory_monitor = DirectoryMonitor(target_directory, self._file_events)
+        self._delta_transfer_cache = DeltaTransferCache(target_directory, self._file_events)
 
     def start(self) -> None:
         self._logger.info(f"Starting Sync Engine Client. Monitoring directory: {self._target_directory}")
@@ -20,10 +23,12 @@ class SyncEngineClient:
             self._logger.error(f"Directory not found or not accessible: {self._target_directory}")
             return
 
+        self._delta_transfer_cache.start()
         self._directory_monitor.start()
         while True:
             time.sleep(100)
 
     def stop(self) -> None:
         self._directory_monitor.stop()
+        self._delta_transfer_cache.stop()
         self._logger.info("Sync Engine Client stopped.")
